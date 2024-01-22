2a9a7597cf779ec51f0f4c64524c4d8ec29d86f1
mlufong <144293166+mlufong@users.noreply.github.com>
2024-01-22 06:05:25 -0600

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -59,10 +59,10 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = join("", [var.name, "-", "iam-instance-profile"])
-  role = var.iam_role_name
-}
+#resource "aws_iam_instance_profile" "instance_profile" {
+#  name = join("", [var.name, "-", "iam-instance-profile"])
+#  role = var.iam_role_name
+#}
 
 
 
@@ -72,7 +72,7 @@ resource "aws_instance" "web_server" {
   key_name               = var.key_pair_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   user_data              = file("scripts/userdata.sh")
-  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
+#  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
   tags                   = merge(var.tags, { Name = join("", [var.name, "-", "webserver"]) }, { Environment = var.name })
 
   # best practices as per checkov scanner
